3c9679742a87981fa0385d086068696ea8a445e0
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-10-02 17:15:02 -0400

    different types of potion

---
+++ __test__/Potion.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('expect');
+
+const Potion = require('../lib/Potion.js');
+
+
+test('creates a health potion attack', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+
+test('creates a random potion object', () => {
+    const potion = new Potion();
+
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+})
