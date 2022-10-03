10585197ae8c7b68f713814c33d6ccd20f70717b
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-10-02 23:52:28 -0400

    health

---
+++ __test__/Player.test.js
@@ -0,0 +1,84 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+const { expect } = require('expect');
+const Player = require('../lib/Player');
+
+
+console.log(new Potion());
+
+
+test ('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)]));
+});
+
+
+test ("gets a player's stats as an object", () => {
+    const player = new Player ('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+
+test ("gets inventory from a player or return false", () => {
+    const player = new Player ('Dave');
+    
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
+
+test ("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+
+test ('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+    
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0
+
+    expect(player.isAlive()).toBeFalsy();
+
+});
+
+test("subtracts from player's health", () => {
+    const player = new Player ('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+    
+    expect(player.health).toBe(0);
+});
+
+
+
+
+/*The expect.stringContaining() method is an expect method that we can use to make sure our
+ string includes our player's health*/
+
+
+ // note truthy values are values that will be true in Boolean contexts such as inside if statements
+ // Hence false being in else stataments
